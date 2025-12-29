575819ff5756f58631db8f593defca0c4e151846
Amaan0001-hub <amaan.pulsepromote@gmail.com>
2025-12-29 22:36:06 +0530

    Parcharadda-3

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,18 +1,48 @@
+// const nodemailer = require("nodemailer");
+
+// module.exports = async (name, email, phone, message) => {
+//  const transporter = nodemailer.createTransport({
+//   host: process.env.SMTP_HOST,
+//   port: process.env.SMTP_PORT,
+//   secure: false, 
+//   auth: {
+//     user: process.env.SMTP_USER, 
+//     pass: process.env.SMTP_PASS, 
+//   },
+// });
+
+//   await transporter.sendMail({
+//     from: `" Contact to Parcharadda" <${process.env.ADMIN_EMAIL}>`,
+//     to: process.env.ADMIN_EMAIL,
+//     subject: "New Contact Form Submission",
+//     text: `
+// Name: ${name}
+// Email: ${email}
+// Phone: ${phone}
+// Message: ${message}
+// `,
+//   });
+// };
 const nodemailer = require("nodemailer");
 
 module.exports = async (name, email, phone, message) => {
- const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, 
-  auth: {
-    user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASS, 
-  },
-});
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false, // 587 ke liye
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  await transporter.verify(); // SMTP test
 
   await transporter.sendMail({
-    from: `" Contact to Parcharadda" <${process.env.ADMIN_EMAIL}>`,
+    from: `"Contact to Parcharadda" <${process.env.SMTP_USER}>`,
     to: process.env.ADMIN_EMAIL,
     subject: "New Contact Form Submission",
     text: `
